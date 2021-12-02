45aff026f39ec670575d94fa474296ebd49dac32
Sohma-Tanaka <tanaka_souma@live2d.com>
2021-12-02 18:05:22 +0900

    コピーライトの追加し忘れを修正

--- Assets/Scripts/PostEffectsManager.cs
+++ Assets/Scripts/PostEffectsManager.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/**
+* Copyright(c) Live2D Inc. All rights reserved.
+*
+* Use of this source code is governed by the Live2D Open Software license
+* that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
